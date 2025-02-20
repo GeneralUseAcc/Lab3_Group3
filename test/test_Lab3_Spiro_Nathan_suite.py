a5d329607483b5fdbb43e0b15f431650c89565ed
GeneralUseAcc <91762522+GeneralUseAcc@users.noreply.github.com>
2025-02-19 21:09:43 -0500

    New stuff

--- test/test_Lab3_Spiro_Nathan_suite.py
+++ test/test_Lab3_Spiro_Nathan_suite.py
@@ -1,6 +1,5 @@
 import unittest
 # from unittest import suite
-
 from test.test_Lab3_Spiro_Nathan import TestArea
 
 
@@ -12,15 +11,13 @@ def shape_test_suite():
     # suite.addTest(TestEmployee('test_email'))
 
     while run_tests:
+        user_input = input("Please enter one of the following options:\n- 'c' for testing area of circle\n- 't' for testing area of trapezium\n- 'e' for testing area of ellipse\n- 'r' for testing area of rhombus\n- 'q' to quit\nWhat would you like to do?: ")
         if user_input == 'c':
             shape_suite.addTest(TestArea('test_circle_01'))
             # You can make suite of entire test class too
             shape_suite.addTest(unittest.makeSuite(TestArea))
             runner = unittest.TextTestRunner()
             print(runner.run(shape_suite))
-
-
-
 
 
 shape_test_suite()
