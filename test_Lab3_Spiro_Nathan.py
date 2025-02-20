fa10e9254e833ca170411c247a4e62670b8fbf3c
GeneralUseAcc <91762522+GeneralUseAcc@users.noreply.github.com>
2025-02-19 19:40:28 -0500

    idk

---
+++ test_Lab3_Spiro_Nathan.py
@@ -0,0 +1,27 @@
+import unittest
+from Lab3_Spiro_Nathan import circle_area, trapezium_area, ellipse_area, rhombus_area
+
+class TestCircleArea(unittest.TestCase):
+    def setUpClass(self):
+        print("setUpClass\n")
+
+    def setUp(self):
+        print("setUp")
+
+        #TODO: Write test cases here
+    def test_radius_zero(self):
+
+    def tearDown(self):
+        print("tearDown")
+
+    def tearDownClass(self):
+        print("tearDownClass")
+
+#class TestTrapeziumArea(unittest.TestCase):
+
+#class TestEllipseArea(unittest.TestCase):
+
+#class TestRhombusArea(unittest.TestCase):
+
+
+
