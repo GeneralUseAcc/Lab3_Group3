857f91bd427e334f09ebda6c9adc4a771ceb842b
GeneralUseAcc <91762522+GeneralUseAcc@users.noreply.github.com>
2025-02-19 19:59:39 -0500

    please work

--- test/test_Lab3_Spiro_Nathan.py
+++ test/test_Lab3_Spiro_Nathan.py
@@ -1,2 +1,23 @@
+# February 14th, 2025
+# Spiro Kontossoros
+# Python unittest file
+# This is a testing file for app.py.
 
-from app.Lab3_Spiro_Nathan import
+import unittest
+
+from app.Lab3_Spiro_Nathan import circle_area, trapezium_area, ellipse_area, rhombus_area
+
+class TestCircleArea(unittest.TestCase):
+    def setUpClass(cls):
+        print("setUpClass")
+
+    def setUp(self):
+        print("setUp")
+
+
+
+    def tearDown(self):
+        print()
+
+    def tearDownClass(cls):
+        print("tearDownClass")
