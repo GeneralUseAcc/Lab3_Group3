74e428613023bcbd269b46999b80181e3e95234b
Nathan Sheldrake <nathansheldrake151@gmail.com>
2025-02-20 22:54:26 -0500

    added python packages

--- test/test_Lab3_Spiro_Nathan_suite.py
+++ test/test_Lab3_Spiro_Nathan_suite.py
@@ -5,7 +5,6 @@ from test.test_Lab3_Spiro_Nathan import TestArea
 
 def shape_test_suite():
     run_tests = True
-    shape_suite = unittest.TestSuite()
     user_input = ""
     # You can add individual test methods to the suite too
     # suite.addTest(TestEmployee('test_email'))
@@ -17,26 +16,39 @@ def shape_test_suite():
                            "'e' for testing area of ellipse\n- "
                            "'r' for testing area of rhombus\n- "
                            "'q' to quit\n"
-                           "What would you like to do?: ").lower
+                           "What would you like to do?: ")
 
+        shape_suite = unittest.TestSuite()
         if user_input == 'c':
             shape_suite.addTest(TestArea('test_circle_01'))
-            # You can make suite of entire test class too
-            shape_suite.addTest(unittest.makeSuite(TestArea))
+            shape_suite.addTest(TestArea('test_circle_02'))
+            shape_suite.addTest(TestArea('test_circle_03'))
             runner = unittest.TextTestRunner()
             print(runner.run(shape_suite))
         elif user_input == 't':
             shape_suite.addTest(TestArea('test_trapezium_01'))
+            shape_suite.addTest(TestArea('test_trapezium_02'))
+            shape_suite.addTest(TestArea('test_trapezium_03'))
+            runner = unittest.TextTestRunner()
+            print(runner.run(shape_suite))
         elif user_input == 'e':
             shape_suite.addTest(TestArea('test_ellipse_01'))
+            shape_suite.addTest(TestArea('test_ellipse_02'))
+            shape_suite.addTest(TestArea('test_ellipse_03'))
+            runner = unittest.TextTestRunner()
+            print(runner.run(shape_suite))
         elif user_input == 'r':
             shape_suite.addTest(TestArea('test_rhombus_01'))
+            shape_suite.addTest(TestArea('test_rhombus_02'))
+            shape_suite.addTest(TestArea('test_rhombus_03'))
+            runner = unittest.TextTestRunner()
+            print(runner.run(shape_suite))
         elif user_input == 'q':
             print("Exiting Program")
-            quit()
+            run_tests = False
         else:
             print("Error: Invalid input, please try again")
-            continue
+
 
 if __name__ == '__main__':
     shape_test_suite()
