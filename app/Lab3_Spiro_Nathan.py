673ee65a7297b3a49310d0710573724d454433dd
GeneralUseAcc <91762522+GeneralUseAcc@users.noreply.github.com>
2025-02-20 22:06:34 -0500

    ok

--- app/Lab3_Spiro_Nathan.py
+++ app/Lab3_Spiro_Nathan.py
@@ -12,39 +12,39 @@ class ShapeArea(object):
         """Calculates the area of a circle given the radius"""
         if not isinstance(r, (int)):
             raise ValueError("Input must be an integer")
-        elif (r <= 0):
+        elif r <= 0:
             raise ValueError("Input cant be negative")
         return round(pi * (r ** 2), 2)
 
     def trapezium_area(self, a, b, h):
         """Calculates the area of a trapezium given base A, base B and the height"""
-        if not isinstance(a, (int)):
+        if not isinstance(a, int):
             raise ValueError("Input must be an integer")
-        elif not isinstance(b, (int)):
+        elif not isinstance(b, int):
             raise ValueError("Input must be an integer")
-        elif not isinstance(h, (int)):
+        elif not isinstance(h, int):
             raise ValueError("Input must be an integer")
-        elif (a or b or h <= 0):
+        elif a or b or h <= 0:
             raise ValueError("Input cant be negative")
         return round(((( a + b ) / 2) * h), 2)
 
     def ellipse_area(self, a, b):
         """Calculates the area of an ellipse given a and b"""
-        if not isinstance(a, (int)):
+        if not isinstance(a, int):
             raise ValueError("Input must be an integer")
-        elif not isinstance(b, (int)):
+        elif not isinstance(b, int):
             raise ValueError("Input must be an integer")
-        elif (a or b <= 0):
+        elif a or b <= 0:
             raise ValueError("Input cant be negative")
         return round((pi * a * b), 2)
 
     def rhombus_area(self, p, q):
         """Calculates the area of a rhombus given p and q"""
-        if not isinstance(p, (int)):
+        if not isinstance(p, int):
             raise ValueError("Input must be an integer")
-        elif not isinstance(q, (int)):
+        elif not isinstance(q, int):
             raise ValueError("Input must be an integer")
-        elif (p or q <= 0):
+        elif p or q <= 0:
             raise ValueError("Input cant be negative")
         return round(((p * q) / 2), 2)
 
